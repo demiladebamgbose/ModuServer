6f4d13fd04c303193e461f3be8df3a0047503e2c
demiladebamgbose <demiladebamgbose94@gmail.com>
2018-02-06 12:05:21 +0100

    set folder structure

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require ('express');
 const bodyParser = require('body-parser');
-const util = require('./util/Util');
+const util = require('./server/util/Util');
+const morgan = require ('morgan');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(morgan(('dev')));
 
-app.get('/', (res, req) => {
+app.get('/', (req, res) => {
     util.log('info', 'root');
+    res.json({
+        message: 'root route'
+    });
 });
 
 app.listen(port, () => {
