7712255a19c1cd3266bfff5f377af7d548a43b53
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2018-05-01 10:18:17 +0100

    Check if duplicate data exists in personfollow before save

--- common/models/person-follow.js
+++ common/models/person-follow.js
@@ -1,5 +1,16 @@
-
-
 module.exports = function (PersonFollow) {
+  PersonFollow.observe('before save', async (ctx, next) => {
+    const data = ctx.instance || ctx.data;
+    const existingData = await PersonFollow.findOne({
+      where: {
+        and: [{ requesterId: data.requesteeId }, { requesteeId: data.requesterId }],
+      },
+    });
 
+    if (!existingData) {
+      return next();
+    }
+
+    throw (new Error('data exists in the database'));
+  });
 };
