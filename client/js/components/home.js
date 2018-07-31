e2c32f1891b363ca1c6d86a167cf27915c9ef65d
Oreofeoluwapo Ralph Olutola <oreofeoluwapo.olutola@andela.com>
2018-07-31 08:43:22 +0100

    Add header

--- client/js/components/home.js
+++ client/js/components/home.js
@@ -1,14 +1,36 @@
 import SideBar from './sidebar.js';
+import { logOut } from './../services/auth.js';
+import router from './../router/index.js';
 
 export default {
   name: 'Home',
   components: {
     SideBar,
   },
+  methods: {
+    logout() {
+      logOut(localStorage.token)
+      .then(() => {
+        localStorage.clear();
+        router.replace('/auth');
+      });
+    },
+  },
   template: `
-  <div class="row">
+  <div class="row" home-component>
     <SideBar/>
-    <div class="col-md-9 col-md-offset-3">Yeah</div>
+    <div class="col-md-9 col-md-offset-3">
+      <div class="header">
+        <div class="action">
+          <div>
+            <img src="images/cog.svg">
+          </div>
+          <div @click="logout()">
+            <img src="images/logout.svg">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
   `,
 };
